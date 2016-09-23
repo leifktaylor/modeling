4953ad6845aefc33d314e2716717b5c8b92c17cd
blackshe <leiftaylor@gmail.com>
2016-09-22 21:39:55 -0400

    Fix indentation in evolution.py

--- evolution.py
+++ evolution.py
@@ -4,12 +4,12 @@ import random
 
 class Individual(object):
 	def __init__(self, species='', sex='', survival_fitness=0, reproductive_fitness=0, phynotype=[], genotype=[])
-	self.species = species
-	self.sex = sex
-	self.survival_fitness = survival_fitness
-	self.reproductive_fitness = reproductive_fitness
-	self.phenotype = []
-	self.genotype = []
+		self.species = species
+		self.sex = sex
+		self.survival_fitness = survival_fitness
+		self.reproductive_fitness = reproductive_fitness
+		self.phenotype = []
+		self.genotype = []
 	
 	
 def create_individuals(amount):
