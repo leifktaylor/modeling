381365a0eea736bb018775cde84744c78d641d84
blackshe <leiftaylor@gmail.com>
2016-09-22 21:54:43 -0400

    Fix evolution.py
    File now compiles and is importable

--- evolution.py
+++ evolution.py
@@ -1,17 +1,15 @@
-
-
 import random
 
+
 class Individual(object):
-	def __init__(self, species='', sex='', survival_fitness=0, reproductive_fitness=0, phynotype=[], genotype=[])
-		self.species = species
-		self.sex = sex
-		self.survival_fitness = survival_fitness
-		self.reproductive_fitness = reproductive_fitness
-		self.phenotype = []
-		self.genotype = []
-	
-	
+    def __init__(self, species='', sex='', survival_fitness=0, reproductive_fitness=0, phenotype=[], genotype=[]):
+        self.species = species
+        self.sex = sex
+        self.survival_fitness = survival_fitness
+        self.reproductive_fitness = reproductive_fitness
+        self.phenotype = phenotype
+        self.genotype = genotype
+
+
 def create_individuals(amount):
-	species_index = {'test_bird': {'species':'test_bird', }}
-		
+    species_index = {'test_bird': {'species': 'test_bird'}}
