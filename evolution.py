f1d87cab4627ebc5871d3c958b10746a0192fe8d
vonsmash <sekosai@hotmail.com>
2016-09-27 20:51:50 -0400

    Added new code to evolution.py

--- evolution.py
+++ evolution.py
@@ -2,14 +2,41 @@ import random
 
 
 class Individual(object):
-    def __init__(self, species='', sex='', survival_fitness=0, reproductive_fitness=0, phenotype=[], genotype=[]):
+    def __init__(self, species='', sex='', survivability=0, reproductive_likelihood=0, phenotype=[], genotype=[]):
         self.species = species
         self.sex = sex
-        self.survival_fitness = survival_fitness
-        self.reproductive_fitness = reproductive_fitness
+        self.survivability = survivability
+        self.reproductive_likelihood = reproductive_likelihood
         self.phenotype = phenotype
         self.genotype = genotype
+        self.alive = True
 
 
-def create_individuals(amount):
+def create_population(amount):
+    population = []
     species_index = {'test_bird': {'species': 'test_bird'}}
+    sex_list = ('male', 'female')
+    for i in range(0, amount):
+        species = 'test_bird'
+        sex = random.choice(sex_list)
+        survivability = random.randint(0, 100)*.01
+#        print(survivability)
+        reproductive_likelihood = random.randint(0 , 100)*.01
+#        print(reproductive_likelihood)
+        population.append(Individual(species, sex, survivability, reproductive_likelihood))
+    return(population)
+
+def create_adult_population(population):
+    adult_population = []
+    for individual in population:
+        if individualndividual.survivablity < 0.5:
+            individual.alive = False
+    return(adult_population)
+
+def create_next_generation(adult_population):
+    next_generation = []
+    selection_mode = female
+    for individual in adult_population:
+        if individual.alive = True:
+            if selection_mode = female:
+                
