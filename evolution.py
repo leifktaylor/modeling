6b7d651a04fed19c499a9adbc976e7706ba4e14a
vonsmash <sekosai@hotmail.com>
2016-10-27 19:34:20 -0400

    Stuff

--- evolution.py
+++ evolution.py
@@ -1,8 +1,10 @@
+
+
 import random
 
 
 class Individual(object):
-    def __init__(self, species='', sex='', survivability=0, reproductive_likelihood=0, phenotype=[], genotype=[]):
+    def __init__(self, species='', sex='', survivability = 0, reproductive_likelihood = 0, phenotype = [], genotype = []):
         self.species = species
         self.sex = sex
         self.survivability = survivability
@@ -10,19 +12,19 @@ class Individual(object):
         self.phenotype = phenotype
         self.genotype = genotype
         self.alive = True
+        self.hadsex = 0
 
 
 def create_population(amount):
     population = []
     species_index = {'test_bird': {'species': 'test_bird'}}
-
     sex_list = ('male', 'female')
     for i in range(0, amount):
         species = 'test_bird'
         sex = random.choice(sex_list)
-        survivability = random.randint(0, 100)*.01
+        survivability = round(random.uniform(0, 1), 2)
 #        print(survivability)
-        reproductive_likelihood = random.randint(0, 100)*.01
+        reproductive_likelihood = round(random.uniform(0, 1), 2)
 #        print(reproductive_likelihood)
         population.append(Individual(species, sex, survivability, reproductive_likelihood))
     return population
@@ -30,19 +32,51 @@ def create_population(amount):
 
 def create_adult_population(population):
     adult_population = []
+    death_rate = float(.5)
+    death_count = int(len(population)*death_rate)
+    max = float(0)
+    pix = float(0)
     for individual in population:
-        if individualndividual.survivablity < 0.5:
-            individual.alive = False
-    return adult_population
+        max += individual.survivability
+        individual.surv_toprange = max
+        individual.surv_botrange = max - individual.survivability
+        print('Max: {0}, Top Range: {1}, Bot Range {2}'.format(max, individual.surv_toprange, individual.surv_botrange))
+    for i in range(0, death_count):
+        pix = round(random.uniform(0,max), 2)
+        print(pix)
+        print(i)
+        for individual in population:
+            if individual.surv_botrange < pix and pix < individual.surv_toprange:
+                individual.alive = False
+                print('ded')
+    return population
+
+def create_weighted_sex():
+    max_s = float(0)
+    for individual in population:
+        if individual.alive:
+            if selection_mode == female:
+                if individual.sex = male:
+                    max_s += individual.reproductive_likelihood
+                    individual.sex_toprange = max_s
+                    individual.sex_botrange = max_s - individual.reproductive_likelihood
 
 
-def create_next_generation(adult_population):
+
+
+def create_next_generation(population):
     next_generation = []
     selection_mode = female
     for individual in adult_population:
         if individual.alive == True:
             if selection_mode == female:
-                pass
+                while individual.sex == female:
+
+
+
+
+
+
 
 
 def print_population_statistics(population):
@@ -55,6 +89,8 @@ def print_population_statistics(population):
     """
     for individual in population:
         print_individual_statistics(individual)
+        if individual.alive:
+            print('alive')
 
 
 def print_individual_statistics(individual):
@@ -68,11 +104,17 @@ def print_individual_statistics(individual):
     sex = individual.sex
     survivability = individual.survivability
     reproductive_likelihood = individual.reproductive_likelihood
-    print('Species: {0} | Sex: {1} | Survivability {2} | Reproductive_Chance {3}'.format(species, sex, survivability,
-                                                                                         reproductive_likelihood))
+    print('Species: {0} | Sex: {1} | Survivability {2} | Reproductive_Chance {3}'.format(species, sex, survivability, reproductive_likelihood))
 
+
+def print_adult_population_statistics(adult_population):
+    for individual in population:
+        print_individual_statistics(individual)
+        if individual.alive:
+            print('Alive')
 
 def test():
     a = create_population(20)
-    print_population_statistics(a)
-    
+##    print_population_statistics(a)
+    b = create_adult_population(a)
+    print_population_statistics(b)
---
+++ wc.py
@@ -0,0 +1,47 @@
+import random
+
+def run():
+    print('Choose a series of three coin flips, i.e. heads, heads, tails.  The computer will try to choose better than you such that its pattern will occur first in a set of coin flips.')
+    choice0 = int(input('First choice; Heads = 1, Tails = 0: '))
+    choice1 = int(input('Second choice; Heads = 1, Tails = 0: '))
+    choice2 = int(input('Third choie; Heads = 1, Tails = 0: '))
+    bestof = int(input('Best out of how many rounds? '))
+    won = int(bestof/2) + 1
+    cc0 = 0
+    cc1 = choice0
+    cc2 = choice1
+    if choice1 == 0:
+        cc0 = 1
+    print('Players choice 1: {0}, 2: {1}, 3: {2} || Computer choice 1: {3}, 2: {4} 3: {5}'.format(choice0, choice1, choice2, cc0, cc1, cc2))
+    player_count = 0
+    computer_count = 0
+    stop = 1
+    s = 0
+    f = []
+    for x in range (0, bestof):
+        if computer_count == won:
+            print('You have lost to the computer you stupid idiot!')
+            break
+        if player_count == won:
+            print('You beat the computer you lucky duck!')
+            break
+        for y in range (0, stop):
+            s = int(random.randint(0, 1))
+            f.append(s)
+            print(f)
+            if stop >= 2:
+                if f[y - 2] == choice0 and f[y - 1] == choice1 and f[y] == choice2:
+                    player_count += 1
+                    print('Player has won a round')
+##                    f.clear()
+##                    stop = 1
+                    break
+                if f[y - 2] == cc0 and f[y - 1] == cc1 and f[y] == cc2:
+                    computer_count += 1
+                    print('Computer has won a round')
+##                    f.clear()
+##                    stop = 1
+                    break
+                else:
+                    stop += 1
+            stop += 1
